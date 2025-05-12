4662204aa053dc8f24dcbc9c78d906a7bad0c218
KimYeonBee <yeonbee0427@gmail.com>
2025-05-13 03:29:11 +0900

    first commit

--- project/static/js/main/main_page.js
+++ project/static/js/main/main_page.js
@@ -1,1 +1,18 @@
 /* main page - JS */
+// main.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    const tabs = document.querySelectorAll('.selection');
+
+    console.log("✅ JS 로드됨, selection 개수:", tabs.length);
+
+    tabs.forEach(tab => {
+        tab.addEventListener('click', function (e) {
+            e.preventDefault(); // 새로고침 방지
+            console.log("클릭됨:", this.textContent); // 콘솔 확인용
+
+            tabs.forEach(t => t.classList.remove('selected'));
+            this.classList.add('selected');
+        });
+    });
+});
