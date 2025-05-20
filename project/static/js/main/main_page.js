83850a2b3e2ee7ef99a3d97c3dd9c27b5e43bfc8
KimYeonBee <yeonbee0427@gmail.com>
2025-05-20 22:06:16 +0900

    first commit

--- project/static/js/main/main_page.js
+++ project/static/js/main/main_page.js
@@ -16,3 +16,90 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+
+const cateBar = document.getElementById("cateBar");
+const checkboxes = document.querySelectorAll(".checkbox");
+
+
+cateBar.style.display = "none";
+
+checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener("change", function () {
+        const parent = checkbox.closest(".cate-select");
+        const cateText = parent.querySelector(".cateText")
+        const text = cateText.textContent.trim();
+
+        // 체크되었을 때만 추가
+        if (checkbox.checked) {
+            const cateHead = document.createElement("div");
+            cateHead.className = "cate-head-item";
+
+            const textElement = document.createElement("p");
+            textElement.className = "cate-head-text";
+            textElement.textContent = text;
+
+            const deleteBtn = document.createElement("img");
+            deleteBtn.src = "/static/assets/icons/close.svg";
+            deleteBtn.style.width = "14px";
+            deleteBtn.style.cursor = "pointer";
+
+            deleteBtn.addEventListener("click", () => {
+                cateHead.remove();
+                checkbox.checked = false;
+
+                const remaining = cateBar.querySelectorAll(".cate-head-item").length;
+                if (remaining === 0) {
+                    cateBar.style.display = "none";
+                }
+            });
+
+            cateHead.appendChild(textElement);
+            cateHead.appendChild(deleteBtn);
+            cateBar.appendChild(cateHead);
+            cateBar.style.display = "flex";
+            updateTotalCount();
+            console.log("추가됨:", text);
+
+        } else {
+            // 체크 해제 시 해당 텍스트 제거
+            const items = cateBar.querySelectorAll(".cate-head-item");
+            items.forEach((item) => {
+                if (item.textContent.trim() === text) {
+                    item.remove();
+                    console.log("제거됨:", text);
+                }
+            });
+
+            if (cateBar.querySelectorAll(".cate-head-item").length === 0) {
+                cateBar.style.display = "none"; //
+            }
+            updateTotalCount();
+        }
+    });
+});
+
+const reset = document.querySelector(".reset");
+reset.addEventListener("click", () => {
+    checkboxes.forEach(cb => (cb.checked = false));
+    cateBar.innerHTML = "";
+    cateBar.style.display = "none";
+    updateTotalCount();
+});
+
+
+// 총합수
+
+function updateTotalCount() {
+    const checkedBoxes = document.querySelectorAll(".cate-select .checkbox:checked");
+    let total = 0;
+    checkedBoxes.forEach(checkbox => {
+        const parent = checkbox.closest(".cate-select");
+        const countText = parent.querySelector(".count").textContent;
+        const count = parseInt(countText);
+        total += count;
+    });
+
+    const totalTextEl = document.getElementById("totalCount");
+    totalTextEl.textContent = `총 ${total || 0}건`;
+}
--- project/static/js/main/product_detail_page.js
+++ project/static/js/main/product_detail_page.js
@@ -1,1 +1,32 @@
 /* product detail page - JS */
+const minusBtn = document.getElementById("minus");
+const plusBtn = document.getElementById("plus");
+const totalCount1 = document.getElementById("totalCount");
+let totalCount = parseInt(totalCount1.textContent);
+
+minusBtn.addEventListener("click", () => {
+    if (totalCount <= 1) {
+        return alert("최소 구매 수량은 1개입니다");
+    }
+    else {
+        totalCount -= 1;
+        totalCount1.textContent = totalCount;
+        calculation();
+    }
+}
+)
+
+plusBtn.addEventListener("click", () => {
+    totalCount += 1;
+    totalCount1.textContent = totalCount;
+    calculation();
+})
+
+const price = document.getElementById("price-tag");
+const discountPrice = parseInt(price.dataset.discount);
+
+function calculation(){
+    const totalPrice = discountPrice * totalCount;
+    const totalPrice1 = document.getElementById("totalPrice");
+    totalPrice1.textContent = `${totalPrice}원`;
+}
